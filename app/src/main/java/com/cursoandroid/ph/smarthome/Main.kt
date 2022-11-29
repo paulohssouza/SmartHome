bd34633e68b3c58569bdd082d5e7f470ae4afe3a
Paulo Henrique <34384560+paulohssouza@users.noreply.github.com>
2022-11-29 14:34:27 -0300

    :contruction: Define constructor

--- app/src/main/java/com/cursoandroid/ph/smarthome/Main.kt
+++ app/src/main/java/com/cursoandroid/ph/smarthome/Main.kt
@@ -1,7 +1,7 @@
 package com.cursoandroid.ph.smarthome
 
 fun main () {
-    val device = SmartDevice()
+    val device = SmartDevice(name = "Android TV", category = "Entertaiment")
     println("Device name is: ${device.name}")
     device.turnOn()
     device.turnOff()
--- app/src/main/java/com/cursoandroid/ph/smarthome/SmartDevice.kt
+++ app/src/main/java/com/cursoandroid/ph/smarthome/SmartDevice.kt
@@ -1,9 +1,14 @@
 package com.cursoandroid.ph.smarthome
 
-class SmartDevice {
-    val name = "Android TV"
-    val category = "Entertaiment"
+class SmartDevice (val name: String, val category: String){
     var deviceStatus = "online"
+    constructor(name: String, category: String, statusCode: Int) : this(name, category) {
+        deviceStatus = when (statusCode) {
+            0 -> "offline"
+            1 -> "online"
+            else -> "unknown"
+        }
+    }
     fun turnOn () {
         println("Smart device is turned on.")
     }
