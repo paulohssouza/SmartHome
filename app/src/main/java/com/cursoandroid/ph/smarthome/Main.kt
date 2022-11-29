be6133f964dd34a03e93882ff9d9222f255cfd00
Paulo Henrique <34384560+paulohssouza@users.noreply.github.com>
2022-11-29 14:20:39 -0300

    :contruction: Set class properties

--- app/src/main/java/com/cursoandroid/ph/smarthome/Main.kt
+++ app/src/main/java/com/cursoandroid/ph/smarthome/Main.kt
@@ -2,6 +2,7 @@ package com.cursoandroid.ph.smarthome
 
 fun main () {
     val device = SmartDevice()
+    println("Device name is: ${device.name}")
     device.turnOn()
     device.turnOff()
 }
--- app/src/main/java/com/cursoandroid/ph/smarthome/SmartDevice.kt
+++ app/src/main/java/com/cursoandroid/ph/smarthome/SmartDevice.kt
@@ -1,6 +1,9 @@
 package com.cursoandroid.ph.smarthome
 
 class SmartDevice {
+    val name = "Android TV"
+    val category = "Entertaiment"
+    var deviceStatus = "online"
     fun turnOn () {
         println("Smart device is turned on.")
     }
