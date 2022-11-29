bd3c543b547446ca1dbb6e3dfd1bbe2c7875def7
Paulo Henrique <34384560+paulohssouza@users.noreply.github.com>
2022-11-29 15:52:24 -0300

    :checkered_flag: Test the solution

--- app/src/main/java/com/cursoandroid/ph/smarthome/Main.kt
+++ app/src/main/java/com/cursoandroid/ph/smarthome/Main.kt
@@ -1,8 +1,21 @@
 package com.cursoandroid.ph.smarthome
 
 fun main () {
-    var device: SmartDevice = SmartTvDevice ("Android TV", "Entertaiment")
-    device.turnOn()
-    device = SmartLightDevice ("Google Light", "Utility")
-    device.turnOn()
+    val smartHome = SmartHome(
+        SmartTvDevice(deviceName = "Android TV", deviceCategory = "Entertainment"),
+        SmartLightDevice(deviceName = "Google light", deviceCategory = "Utility")
+    )
+
+    smartHome.turnOnTv()
+    smartHome.turnOnLight()
+    println("Total number of devices currently turned on: ${smartHome.deviceTurnOnCount}")
+    println()
+
+    smartHome.increaseTvVolume()
+    smartHome.changeTvChannelToNext()
+    smartHome.increaseLightBrightness()
+    println()
+
+    smartHome.turnOffAllDevices()
+    println("Total number of devices currently turned on: ${smartHome.deviceTurnOnCount}.")
 }
